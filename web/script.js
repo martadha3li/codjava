c575b03f468148b8e19939430c75d3fdd03707e6
martadha3li <martadha3li@gmail.com>
2025-04-23 10:44:02 +0300

    Create script.js

---
+++ web/script.js
@@ -0,0 +1,5 @@
+function runCode() {
+  const javaCode = document.getElementById('javaCode').value;
+  const result = document.getElementById('result');
+  result.textContent = `كود جافا الذي تم إدخاله:\n${javaCode}`;
+}
